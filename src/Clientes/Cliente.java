f7482925c4a8229bd753b63a954b7370838153d0
christianacuna <christianlorenso@hotmail.com>
2020-08-18 00:35:04 -0600

    Implementacion extra sin errores.

--- src/Clientes/Cliente.java
+++ src/Clientes/Cliente.java
@@ -1,40 +1,41 @@
 package Clientes;
 
-import Clientes.GUI.MsjBox;
 import Clientes.GUI.Table;
-
+import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.ConnectException;
 import java.net.Socket;
 import java.util.Scanner;
 
 import static java.lang.Thread.sleep;
 
-public class Cliente {
+public class Cliente extends JFrame{
+    private String texto;
     public static void main(String[] args) throws Exception {
-        System.out.println("Indique cual IP quiere conectarse y a cual puerto");
-        Scanner scanner = new Scanner(System.in);
-        String ip = scanner.nextLine();
-        String[] address = ip.split(":");
+        String direccion;
+        direccion = JOptionPane.showInputDialog("Direccion server IP:PUERTO");
+        //Scanner scanner = new Scanner(System.in);
+        //String ip = scanner.nextLine();
+        String[] address = direccion.split(":");
         Table frame = new Table();
-        MsjBox msjBox = new MsjBox();
-        msjBox.display();
-        msjBox.waitUntilready();
-        System.out.println(msjBox.getCampotexto());
         try (Socket socket = new Socket(address[0],Integer.parseInt(address[1]))) {
             System.out.println("Enter lines of text then Ctrl+D or Ctrl+C to quit");
             var in = new Scanner(socket.getInputStream());
             var out = new PrintWriter(socket.getOutputStream(), true);
             BufferedReader in2 = new BufferedReader( new InputStreamReader( socket.getInputStream() ) );
 
-            System.out.println(scanner.hasNextLine());
+            //System.out.println(scanner.hasNextLine());
             System.out.println(socket.isConnected());
             System.out.println("--");
             String entrada = "No Connection";
             String entrada2 = entrada;
-
-            while (scanner.hasNextLine() && socket.getInputStream().read() != -1) {
+            if(socket.getInputStream().read() != -1){
+                frame.display(entrada);
+            }
+            /*
+            while (socket.getInputStream().read() != -1) {
                 //System.out.println(socket.isConnected());
                 //System.out.println(socket.isClosed());
                 //System.out.println(socket.isBound());
@@ -44,14 +45,15 @@ public class Cliente {
                 while(!in2.ready()){
                     sleep(100);
                 }
-                String linea = scanner.nextLine();
+                //String linea = scanner.nextLine();
                 entrada = in.nextLine();
                 entrada2 = in2.readLine();
-                System.out.println(linea);
+                //System.out.println(linea);
                 System.out.println(entrada);
-            }
-            frame.display(entrada);
+            }*/
             System.out.println("Conexion terminada");
+        }catch (ConnectException e){
+            System.out.println("Servidor "+direccion+" No encontrado");
         }
     }
 }
