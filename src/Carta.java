ba34ebde52dfe63527c8c881ea3148a6185acc32
christianacuna <christianlorenso@hotmail.com>
2020-08-17 20:55:20 -0600

    Implementacion extra sin errores.

--- src/Carta.java
+++ src/Carta.java
@@ -1,10 +1,29 @@
 public class Carta implements Comparable<Carta>{
-    private String color;
     private String palo;
     private int numero;
     private boolean visible;
+    public Carta(String palo,int numero,boolean visible){
+        this.palo=palo;
+        this.numero=numero;
+        this.visible=visible;
+    }
+
+    public void setVisible(boolean visible) {
+        this.visible = visible;
+    }
 
     public String getColor() {
+        String color;
+        switch(this.palo) {
+            case "D":
+                color = "red";
+                break;
+            case "H":
+                color = "red";
+                break;
+            default:
+                color = "black";
+        }
         return color;
     }
 
